81606fe6725238dc7e3c1d5b68eaa04d6688fee9
interpreted-keyword-9673 <mel.cantorias@gmail.com>
2020-03-06 16:45:51 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,21 @@
 # code your #position_taken? method here!
+
+
+def position_taken?(board, index)
+  if board[index] == " " || ""
+    return false
+    if board[index] == "X" || "O"
+      return true
+      if board[index] == "X"
+        return true
+      else
+        return false
+      end
+    else
+      return false
+    end
+  else
+      return true
+  end
+end
+  
